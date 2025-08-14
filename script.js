ac6929ae068d52240d25a14fe43083827da1ccf1
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-14 14:30:32 +0530

    👍4

--- script.js
+++ script.js
@@ -9,10 +9,18 @@
 // let result = 35 * 2 - (10 / 2) + 7;
 // console.log(result);
 //thrid question Log the data type of "123", 123, true, and null using typeof.
-console.log(typeof "123");
-console.log(typeof 123);
-console.log(typeof true);
-console.log(typeof null);
+// console.log(typeof "123");
+// console.log(typeof 123);
+// console.log(typeof true);
+// console.log(typeof null);
+// fourth question . Write a program that swaps the values of two variables.
+let a = 10
+let b = 12
+let c;
+c = a;
+a = b
+b = c
+console.log(a,b);
 
 
 
