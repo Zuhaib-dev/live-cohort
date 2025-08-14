20f3bc01bb92b2fd8b35bc9f6b89383856f0d0b2
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-14 15:00:31 +0530

    done with the 12th

--- script.js
+++ script.js
@@ -15,3 +15,26 @@
 //     i--
     
 // }while(i>=1)
+//12 Use a while loop to print multiples of 3 from 3 to 30
+// let i = 3
+// while(i<=30){
+//     console.log(i);
+//     i += 3
+    
+// }
+// let i = 4
+// while(i<=40){
+//     console.log(i);
+//     i+=4
+    
+// }
+// let i = 3
+// do{
+//     console.log(i);
+//     i+=3
+    
+// }while(i<=30)
+// for(let i = 3;i<=30;i+=3){
+//     console.log(i);
+    
+// }
