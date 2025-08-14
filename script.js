eb7b83834c033db8f318de04fff424c0402553bb
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-14 15:05:48 +0530

    13th

--- script.js
+++ script.js
@@ -1,40 +1,63 @@
 // 11. Write a for loop to print numbers from 10 to 1 in reverse.
 //  for(let i = 10;i>=1;i--){
 //     console.log(i);
-    
+
 //  }
 // let i = 10
 // while(i>=1){
 //     console.log(i);
 //     i--
-    
+
 // }
 // let i = 10
 // do{
 //     console.log(i);
 //     i--
-    
+
 // }while(i>=1)
 //12 Use a while loop to print multiples of 3 from 3 to 30
 // let i = 3
 // while(i<=30){
 //     console.log(i);
 //     i += 3
-    
+
 // }
 // let i = 4
 // while(i<=40){
 //     console.log(i);
 //     i+=4
-    
+
 // }
 // let i = 3
 // do{
 //     console.log(i);
 //     i+=3
-    
+
 // }while(i<=30)
 // for(let i = 3;i<=30;i+=3){
 //     console.log(i);
+
+// }
+// 13. Write a program to calculate the sum of numbers from 1 to 100 using a loop
+// let sum = 0
+// for(let i = 0;i<=100;i++){
+//     sum+=i
+//     console.log(sum);
+
+// }
+// let sum = 0
+// let i = 0
+// while(i<=100){
+//     sum+=i
+//     console.log(sum);
+//     i++
     
 // }
+// let sum = 0 
+// let i = 0
+// do{
+//     sum+=i
+//     console.log(sum);
+//     i++
+    
+// }while(i<=100)
