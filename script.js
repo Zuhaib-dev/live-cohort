982ec13f3b2f022cdfa478cc3fb554a354d4cd73
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-14 14:57:31 +0530

    done with the 11

---
+++ script.js
@@ -0,0 +1,17 @@
+// 11. Write a for loop to print numbers from 10 to 1 in reverse.
+//  for(let i = 10;i>=1;i--){
+//     console.log(i);
+    
+//  }
+// let i = 10
+// while(i>=1){
+//     console.log(i);
+//     i--
+    
+// }
+// let i = 10
+// do{
+//     console.log(i);
+//     i--
+    
+// }while(i>=1)
