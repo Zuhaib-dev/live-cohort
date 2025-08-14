d7ff9c2fcd262407eb38b1b132093fe33c16113e
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-14 14:26:30 +0530

    done with the second question also

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // first question log to the console by various methods
-console.log("Hello World");
-console.warn("Hello World");
-console.error("Hello World");
-console.info("Hello World");
-console.table({log:"Hello world"})
+// console.log("Hello World");
+// console.warn("Hello World");
+// console.error("Hello World");
+// console.info("Hello World");
+// console.table({log:"Hello world"})
+  
+//second question Perform 35 * 2 - (10 / 2) + 7 and log the result.
+let result = 35 * 2 - (10 / 2) + 7;
+console.log(result);
