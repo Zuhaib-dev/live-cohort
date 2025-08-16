cdf3362a72ba857cda599de441c4e728cf143587
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-16 10:09:27 +0530

    done with another of forof loop

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
-// 1. Write a for loop to print numbers from 10 to 1 in reverse.
-// let i = 10;
-// while (i >= 1) {
-//   console.log(i);
-//   i--;
-// }
-// for(let i =10;i>=1;i--){
-//     console.log(i);
-
-// }
-// Use a while loop to print multiples of 3 from 3 to 30
-// let i = 3;
-// while (i <= 30) {
-//   console.log(i);
-//   i += 3;
-// }
-// Write a program to calculate the sum of numbers from 1 to 100 using a loop
-// let sum = 0;
-// for (let i = 1; i <= 100; i++) {
-//   sum += i;
-//   console.log(sum);
+// //15. Use a for...of loop to iterate over the string "JavaScript"
+// let str = "javaScript"
+// for(let char of  str){
+//     console.log(char);
+    
 // }
