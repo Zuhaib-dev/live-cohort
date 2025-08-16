cc9b76f1879d2fc6c115eaa0a346106874363a0a
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-16 09:40:38 +0530

    done 3 questions from sheet 2 by self other will learn then try

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-// 9. Create an array of 5 numbers and log the sum using .reduce().
-// let arr = [100,1,56,3,8]
-// let ans = arr.reduce(function(acc,num){
-//     return acc + num
-// })
-// console.log(ans);
-// 9. Create an array of 5 numbers and log the multiply using .reduce().
-let arr = [43,54,65,331,54]
-let ans = arr.reduce(function(acc, val){
-    return acc * val
-})
-console.log(ans);
+// 1. Write a for loop to print numbers from 10 to 1 in reverse.
+// let i = 10;
+// while (i >= 1) {
+//   console.log(i);
+//   i--;
+// }
+// for(let i =10;i>=1;i--){
+//     console.log(i);
 
+// }
+// Use a while loop to print multiples of 3 from 3 to 30
+// let i = 3;
+// while (i <= 30) {
+//   console.log(i);
+//   i += 3;
+// }
+// Write a program to calculate the sum of numbers from 1 to 100 using a loop
+// let sum = 0;
+// for (let i = 1; i <= 100; i++) {
+//   sum += i;
+//   console.log(sum);
+// }
