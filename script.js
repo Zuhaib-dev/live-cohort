0a6e76754ddd6e6c6642b63e5e0ba8d1c85e24ab
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-16 09:32:06 +0530

    done with the sheet one

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // 9. Create an array of 5 numbers and log the sum using .reduce().
-let arr = [100,1,56,3,8]
-let ans = arr.reduce(function(acc,num){
-    return acc + num
+// let arr = [100,1,56,3,8]
+// let ans = arr.reduce(function(acc,num){
+//     return acc + num
+// })
+// console.log(ans);
+// 9. Create an array of 5 numbers and log the multiply using .reduce().
+let arr = [43,54,65,331,54]
+let ans = arr.reduce(function(acc, val){
+    return acc * val
 })
 console.log(ans);
+
