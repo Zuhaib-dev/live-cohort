401f14b9f49845466f18a1428d4b10b9a2fc1e12
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-16 11:54:11 +0530

    started the new questions of practice set 3

--- Readme.md
+++ Readme.md
@@ -1,1 +1,2 @@
-
+practice set 2
+https://docs.google.com/document/d/1KCDX9mPKyrcc3nx8PJyETRxi4cdFY2Xbwa6FqdvJ_GA/edit?tab=t.0#heading=h.fk3m9fb4xw6h
--- script.js
+++ script.js
@@ -1,26 +1,7 @@
-// 11. Write a for loop to print numbers from 10 to 1 in reverse
-// for(let i = 10;i>=1;i--){
-//     console.log(i);
-    
-// }
-// Use a while loop to print multiples of 3 from 3 to 30
-// let i = 3
-// while(i<=30){
-//     console.log(i);
-//     i+=3
-    
-    
-// }
-// let sum = 0
-// for(let i = 1; i<=100;i++){
-//     sum+=i
-// }
-// console.log(sum);
-// let str = 'javascript'
-// for(let char of str ){
-//     console.log(char);
-// }
-let arr = [1,1,2,2,3,3]
-let arr2 = [...new Set(arr)]
-console.log(arr2);
-
+// 17. Find the second largest number in an array
+let arr = [1,3,1,3,2,4,2,4,5,4,5,3,1,2,3,4,]
+let uniqArr = [...new Set(arr)]
+uniqArr.sort(function(a,b){
+    return b - a
+})
+console.log(uniqArr[1]);
