c971ecfd287354727b980ec11b3b874371048b90
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-18 09:52:21 +0530

    done with the 3 one also

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
-// Create a function that takes another function as an argument and calls it after 3 seconds (HOF + Callback).
-// Implement your own version of `.map()` as a higher-order function.
-function abcd(fn){
-    setTimeout(fn,3000)
-
+// Write a function that uses closures to create a counter.
+function counter() {
+  let count = 0;
+  return function () {
+    count++;
+    console.log(count);
+  };
 }
-abcd(function(){
-    console.log("Hllo");
-    
-})
-// Implement your own version of `.map()` as a higher-order function.
-let arr = [1,23,3]
-function zap(arr,fnc){
-    let newArr = []
-    for(let i = 0;i<arr.length;i++){
-        newArr.push(fnc(arr[i]))
-    }
-    return newArr
-
-}
- let ans = zap(Array,function(value){
-    return value +2
-})
+let makecount = counter();
+makecount()
+makecount()
+makecount()
+makecount()
