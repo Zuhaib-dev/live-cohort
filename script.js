f01fa309dc1826df33c38b8a6b247bdeded7a792
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-18 11:21:26 +0530

    done with the 3 one also

--- script.js
+++ script.js
@@ -6,10 +6,27 @@
 // barbarChalavoo(function(){console.log("hello");
 // },2000)
 // 2 Implement a function that returns a function with a preset greeting (Closure)
-function greeter(greeting) {
-  return function (name) {
-    console.log(`${greeting} ${name}`);
-  };
+// function greeter(greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// }
+// let fnGreeter = greeter("Hello");
+// fnGreeter("ZUhaib")
+// 3 Implement a function that takes a callback and only executes it once (HOF + Closure).
+function once(fn, limit) {
+  let total = 0;
+  return function(){
+    if (total < limit) {
+    total++;
+    fn();
+  }
+  }
 }
-let fnGreeter = greeter("Hello");
-fnGreeter("ZUhaib")
+let ans = once(function () {
+  console.log("Once Only");
+}, 1);
+ans()
+ans()
+ans()
+ans()
