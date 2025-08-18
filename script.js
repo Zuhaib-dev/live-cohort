768300d7bfcd1ef66d07fba7dcb1f4c8daf851c6
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-18 11:10:12 +0530

    done with the second one also

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-// Create a function that takes a callback and executes it after every `n` seconds indefinitely.
-function barbarChalavoo(fn,time){
-    setInterval(fn,time)
+// 1 Create a function that takes a callback and executes it after every `n` seconds indefinitely.
+// function barbarChalavoo(fn,time){
+//     setInterval(fn,time)
 
+// }
+// barbarChalavoo(function(){console.log("hello");
+// },2000)
+// 2 Implement a function that returns a function with a preset greeting (Closure)
+function greeter(greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
 }
-barbarChalavoo(function(){console.log("hello");
-},2000)
+let fnGreeter = greeter("Hello");
+fnGreeter("ZUhaib")
