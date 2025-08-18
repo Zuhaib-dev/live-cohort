3dd82008b5da67723304cf67dc077a8cd17dcb5c
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-18 14:52:29 +0530

    started the DOM

---
+++ script.js
@@ -0,0 +1,8 @@
+let h1 = document.querySelectorAll("h1");
+h1[0].innerHTML = "The one on zero index";
+h1[0].style.color = 'red'
+h1[1].innerHTML = 'the second on 1 index';
+h1[1].style.color = 'blue'
+h1[2].innerHTML = 'the thrid on 2 index'
+h1[2].style.color = 'seagreen'
+
