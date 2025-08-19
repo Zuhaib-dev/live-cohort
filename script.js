f889e5cb327bd998add989ca43d88570a44efac6
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-19 09:26:16 +0530

    Done with the practice of second one also next will try both

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 let arr = ['CSK','MI','SRH','RCB','KKR','LSG','PBKS','GT','DC','RR',]
 let btn = document.querySelector('button')
 let h1 = document.querySelector('h1')
-btn.addEventListener('click', ()=>{
+btn.addEventListener('click', function(){
   let num = Math.floor(Math.random()*arr.length)
-  let winner = (arr[num]);
+  let winner = arr[num]
   h1.innerHTML = winner
-  
-  
 })
