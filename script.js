105a79b9e02a3c2bde04ea58e9497b3ab65e045a
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-19 08:49:38 +0530

    done with the impletaition of DOM with a micro project

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
 let btn = document.querySelector("button");
-let h5 = document.querySelector("h5");
-let flag = 0;
+let box = document.querySelector("#box");
 btn.addEventListener("click", function () {
-  if (flag == 0) {
-    h5.innerHTML = "Friends";
-    h5.style.color = "green";
-    btn.innerHTML = "Remove Friend";
-    console.log("FriendShip Done");
-    
-    flag = 1
-  }else{
-    h5.innerHTML = 'Strangers'
-    h5.style.color = 'red'
-    btn.innerHTML = 'Add Friend'
-    console.log("Removed Friend Susessfully");
-    
-    flag = 0
-  }
+  let red = Math.floor(Math.random()*256)
+  let green = Math.floor(Math.random()*256)
+  let blue = Math.floor(Math.random()*256)
+  console.log(red,green,blue);
+  box.style.backgroundColor = `rgb(${red},${green},${blue})`
+  
 });
