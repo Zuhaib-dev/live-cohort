3582d8aa9a54f3cc074a338349abcfe1315c6044
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-19 14:17:21 +0530

    exploring WEB APIs with a micro project

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-let btn = document.querySelector('button')
-let box = document.querySelector('.box')
-btn.addEventListener('click', ()=>{
-  let r = Math.floor(Math.random()*256)
-  let g = Math.floor(Math.random()*256)
-  let b = Math.floor(Math.random()*256)
-  box.style.backgroundColor = `rgb(${r},${g},${b})`
-  
-})
+let btn = document.querySelector("button");
+let h5 = document.querySelector("h5");
+let check = 0;
+btn.addEventListener("click", function () {
+  if (check == 0) {
+    h5.innerHTML = "Request Sending...";
+    h5.style.color = "black";
+    btn.innerHTML = "Adding Friend..."
+    setTimeout(function(){
+      h5.innerHTML = "Request Accepted"
+      h5.style.color = "black"
+    },1500)
+    setTimeout(function () {
+      h5.innerHTML = "Friends";
+      h5.style.color = "green";
+      btn.innerHTML = "Remove Friend";
+      check = 1;
+    }, 3000);
+  } else {
+    h5.innerHTML = "Stranger";
+    h5.style.color = "red";
+    btn.innerHTML = "Add Friend";
+    check = 0;
+  }
+});
