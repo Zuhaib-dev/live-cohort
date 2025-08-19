44146e45a2f5056cef35b8fd77842c9fcf8fdb95
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-19 09:18:16 +0530

    done with the second project also of DOM now will practice the DOM for more

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+let arr = ['CSK','MI','SRH','RCB','KKR','LSG','PBKS','GT','DC','RR',]
 let btn = document.querySelector('button')
-let box = document.querySelector('#box')
+let h1 = document.querySelector('h1')
 btn.addEventListener('click', ()=>{
-let r = Math.floor(Math.random()*256)
-let g = Math.floor(Math.random()*256)
-let b = Math.floor(Math.random()*256)
-  box.style.backgroundColor = `rgb(${r},${g},${b})`
-
+  let num = Math.floor(Math.random()*arr.length)
+  let winner = (arr[num]);
+  h1.innerHTML = winner
+  
+  
 })
