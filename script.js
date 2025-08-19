ce9b26dd420248333ddd8d0dd31f615cdbed8865
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-19 15:23:00 +0530

    done with the practice also

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 let btn = document.querySelector("button");
-let prg = document.querySelector("#progress");
+let persent = document.querySelector("#persent");
 let growth = document.querySelector("#growth");
 let grow = 0;
-btn.addEventListener("click", function () {
-  let limiter = setInterval(function () {
-    console.log(grow);
-    grow++
-    persent.innerHTML = grow+"%"
-    growth.style.width = grow+'%' 
-  }, 50);
-  setTimeout(function(){
-    clearInterval(limiter)
-    btn.innerHTML = "Downloaded"
-    btn.style.opacity = 0.5
+btn.addEventListener("click", () => {
+  let limiter = setInterval(() => {
+    grow++;
+    persent.innerHTML = grow + "%";
+    growth.style.width = grow + "%";
+  }, 40);
+  setTimeout(() => {
+    clearInterval(limiter);
+    btn.innerHTML = "Downloaded";
+    btn.style.color = 'black'
+    btn.style.opacity = "0.3";
+    btn.style.cursor = 'not-allowed'
     grow = 0
-  },5000)
+  }, 4000);
 });
