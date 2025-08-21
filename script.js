607bf8e394995f0e719fc7122edade1e589446dc
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-21 11:57:45 +0530

    OOPs level 2 prototype

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
-// old way by using functions
-function Toffee(flavor, price, shape) {
-  this.flavor = flavor;
-  this.price = price;
-  this.shape = shape;
-}
-let T1 = new Toffee("Vanilla", 20,"Round");
-let T2 = new Toffee("Chocolate", 30,"Oval");
-let T3 = new Toffee("Strawberry", 40,"CUbe");
-console.log(T1,T2,T3);
-
-//new way using Class and Constructors
-
-class Toffe {
-  constructor(flavor, price) {
-    this.flavor = flavor;
-    this.price = price;
+class Toffee{
+  constructor(name,flavor,shape,ingredients){
+    this.name = name;
+    this.flavor = flavor
+    this.shape = shape
+    this.ingredients = ingredients
   }
 }
-let t1 = new Toffe("Vanilla", 20);
-let t2 = new Toffe("Chocolate", 30);
-let t3 = new Toffe("MangoBite", 40);
-console.log(t1);
-console.log(t2);
-console.log(t3);
+Toffee.prototype.price = "price "+2
+ let t1 = new Toffee("Alpenlaibe","Coffee","Circle","AlmondMilk,sugar,jagre,Coffee")
+ console.log(t1, t1.price);
+ 
