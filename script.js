009b0bd11b26996f376895ca9688891ae1aef5db
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-21 14:28:05 +0530

    Getting in deep with the OOPS with a project

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-class Libarry{
-  
+class Libarary {
+  constructor() {
+    this.books = [];
+  }
+  addBook(book) {
+    this.books.push(book);
+  }
+  listALlBooks() {
+    this.books.forEach(function (book) {
+      console.log(book);
+    });
+  }
 }
+let l1 = new Libarary();
+l1.addBook("The epci Shit");
+l1.addBook("Wealth");
+l1.addBook("poor Father rich son");
+
+l1.listALlBooks();
