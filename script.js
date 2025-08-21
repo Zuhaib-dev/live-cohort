ef747d7894358b54eea037bee9fb263e9ae112f8
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-21 14:12:31 +0530

    Started a Micro Project on OOPs

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-class Employee {
-  constructor(name, post, age, id, salary) {
-    this.name = name;
-    this.post = post;
-    this.age = age;
-    this.id = id;
-    this.salary = salary;
-  }
-}
-Employee.prototype.employeeDetails = function(){
-  console.log(`I be ${this.name} I works as a ${this.post} I'm ${this.age} yers nold . My salary is ${this.salary} `);
+class Libarry{
   
 }
-let e1 = new Employee("Zuhaib Rashid", "Software Develoer", 17, 1119801, 50000);
-let e2 = new Employee("Zuhaib ", "Software Develoer", 18, 1119801, 500000);
-
-
-
