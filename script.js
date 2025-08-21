23f6c38f1d5552968463b174864d985e6cf58687
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-21 14:52:11 +0530

    done with the one micro project

--- script.js
+++ script.js
@@ -6,14 +6,39 @@ class Libarary {
     this.books.push(book);
   }
   listALlBooks() {
-    this.books.forEach(function (book) {
-      console.log(book);
+    this.books.forEach(function (book, index) {
+      console.log(` ${index + 1}  ${book.name} by ${book.author}`);
     });
   }
 }
-let l1 = new Libarary();
-l1.addBook("The epci Shit");
-l1.addBook("Wealth");
-l1.addBook("poor Father rich son");
-
-l1.listALlBooks();
+class Book {
+  constructor(name, isbn, price, author) {
+    this.name = name;
+    this.isbn = isbn;
+    this.price = price;
+    this.author = author;
+    this.readStatus = false;
+  }
+  info() {
+    console.log(
+      ` ${this.readStatus ? "❌" : "✔"} ${this.name} is written by the ${
+        this.author
+      } and you have ${
+        this.readStatus ? "read it" : "you have not read this is available"
+      }  and having isbn no this  ${this.isbn} with the pricing of ${
+        this.price
+      }`
+    );
+  }
+  changeReadStatus() {
+    this.readStatus = !this.readStatus;
+  }
+}
+let jhansiLibarary = new Libarary();
+let book1 = new Book("The Epic Shit", "iqevfiv", 1200, "Nelson Mendela");
+let book2 = new Book("The Epic Shit", "iqevfiv", 1200, "Nelson Mendela");
+let book3 = new Book("The Epic Shit", "iqevfiv", 1200, "Nelson Mendela");
+jhansiLibarary.addBook(book1);
+jhansiLibarary.addBook(book2);
+jhansiLibarary.addBook(book3);
+jhansiLibarary.listALlBooks();
