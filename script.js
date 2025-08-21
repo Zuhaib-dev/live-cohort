ba2ab121e6e6a78bb383039ca08f5f05d509e9d6
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-21 13:59:08 +0530

    Getting into OOPS step by step next will build a  micro Project on oops to get more understanding and for tommorow i will go with the async js and will complete all

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-class Toffee{
-  constructor(name,flavor,shape,ingredients){
+class Employee {
+  constructor(name, post, age, id, salary) {
     this.name = name;
-    this.flavor = flavor
-    this.shape = shape
-    this.ingredients = ingredients
+    this.post = post;
+    this.age = age;
+    this.id = id;
+    this.salary = salary;
   }
 }
-Toffee.prototype.price = "price "+2
- let t1 = new Toffee("Alpenlaibe","Coffee","Circle","AlmondMilk,sugar,jagre,Coffee")
- console.log(t1, t1.price);
- 
+Employee.prototype.employeeDetails = function(){
+  console.log(`I be ${this.name} I works as a ${this.post} I'm ${this.age} yers nold . My salary is ${this.salary} `);
+  
+}
+let e1 = new Employee("Zuhaib Rashid", "Software Develoer", 17, 1119801, 50000);
+let e2 = new Employee("Zuhaib ", "Software Develoer", 18, 1119801, 500000);
+
+
+
