94988f8a070c28a14cd7fc1f7bf142e32847386f
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-22 15:31:31 +0530

    Done with the practice

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-let btn = document.querySelector('#btn3')
-btn.addEventListener('click', function(){
-    function fakeApiCall(endpoint) {
+function fakeApiCall(endpoint) {
   const data = {
     users: ["Zuhaib", "Murtaza"],
-    posts: ["The way of life", "the epic shit"],
+    posts: ["The Epic Shit", "The Wealth"],
   };
-  let delay = Math.random() * 1000 + 1000;
+  let delay = Math.random() * 2000 + 1000;
   return new Promise((resolve, reject) => {
     setTimeout(function () {
       resolve(data[endpoint]);
     }, delay);
   });
 }
-fakeApiCall("users").then(function (data) {
-  console.log(data);
-});
-fakeApiCall("posts").then(function (data) {
-  console.log(data);
-});
-
+fakeApiCall('users')
+.then(function(data){
+    console.log(data);
+    
+})
+fakeApiCall('posts')
+.then(function(data){
+    console.log(data);
+    
 })
