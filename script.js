1b6fb1c34721628afdf642a686d2d35718be2c9e
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-22 15:25:10 +0530

    Done with the another question

--- script.js
+++ script.js
@@ -1,38 +1,22 @@
-function getUser() {
+let btn = document.querySelector('#btn3')
+btn.addEventListener('click', function(){
+    function fakeApiCall(endpoint) {
+  const data = {
+    users: ["Zuhaib", "Murtaza"],
+    posts: ["The way of life", "the epic shit"],
+  };
+  let delay = Math.random() * 1000 + 1000;
   return new Promise((resolve, reject) => {
     setTimeout(function () {
-      resolve({ id: 1, name: "Zuhaib" });
-    }, 1000);
+      resolve(data[endpoint]);
+    }, delay);
   });
 }
-function getPosts(userId) {
-  return new Promise((resolve, reject) => {
-    setTimeout(function () {
-      resolve(["Title 1 ", "Title 2"]);
-    }, 1000);
-  });
-}
-function getCommments(postId) {
-  return new Promise((resolve, reject) => {
-    setTimeout(function () {
-      resolve(["lovely Posts", "Amazinging Content", "niCe"]);
-    }, 1000);
-  });
-}
-getUser()
-  .then(function (data) {
-    console.log(data);
-    return getPosts(data.id);
-  })
-  .then(function (titles) {
-    console.log(titles);
-    return getCommments("dbbb");
-  })
-  .then(function(cmts){
-    console.log(cmts);
-    
-  })
-  .finally(function(){
-    console.log("All ressolved");
-    
-  })
+fakeApiCall("users").then(function (data) {
+  console.log(data);
+});
+fakeApiCall("posts").then(function (data) {
+  console.log(data);
+});
+
+})
