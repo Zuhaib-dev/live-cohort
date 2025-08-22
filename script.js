12b18cd760ad7f7f60df90d4522553cb0cc2bc76
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-22 15:05:02 +0530

    Done with another question

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-let btn = document.querySelector("#btn3");
-btn.addEventListener("click", function () {
-  function orderFood() {
-    return new Promise((resolve, reject) => {
-      setTimeout(function () {
-        let chance = Math.random() < 0.6;
-        if (chance) resolve();
-        else reject();
-      }, 1000);
-    });
-  }
-  orderFood()
-    .then(function () {
-      console.log("✔Fizza Delivered");
-    })
-    .catch(function () {
-      console.log("❌Not Delivered");
-    });
-});
+function getUser() {
+  return new Promise((resolve, reject) => {
+    resolve({ id: 1, name: "Zuhaib" });
+  });
+}
+function getPosts() {
+  return new Promise((resolve, reject) => {
+    resolve(["title 1", "title 2"]);
+  });
+}
+function getComments(postId) {
+  return new Promise((resolve, reject) => {
+    resolve(["greate post", "lovely"]);
+  });
+}
+getUser()
+  .then(function (data) {
+    console.log(data);
+    return getPosts(data.id);
+  })
+  .then(function (titles) {
+    console.log(titles);
+    return getComments("saca")
+      .then(function (cmts) {
+        console.log(cmts);
+      })
+      .finally(function () {
+        console.log("ALL resolved");
+      });
+  });
