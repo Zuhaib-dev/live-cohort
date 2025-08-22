e67a3f6237d8b29d62dda206c7c78adfd1677161
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-22 14:26:01 +0530

    Done with the basics of Promises and also practiced a question which situmilates the Food Delivery with some delay also add chance of not delivering basically res,rej

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-function step1() {
-  return new Promise((resolve, reject) => {
-    console.log("Step 1");
-    resolve();
-  });
-}
-function step2() {
-  return new Promise((resolve, reject) => {
-    console.log("Step 2");
-    resolve();
-  });
-}
-function step3() {
-  return new Promise((resolve, reject) => {
-    console.log("Step 3");
-    resolve();
-  });
-}
-step1().then(step2).then(step3).then(function(){
-    console.log(
-        'All Steps Done'
-    );
-    
-})
+let btn = document.querySelector("#btn3");
+btn.addEventListener("click", function () {
+  function orderFood() {
+    return new Promise((resolve, reject) => {
+      setTimeout(function () {
+        let chance = Math.random() < 0.6;
+        if (chance) resolve();
+        else reject();
+      }, 1000);
+    });
+  }
+  orderFood()
+    .then(function () {
+      console.log("✔Fizza Delivered");
+    })
+    .catch(function () {
+      console.log("❌Not Delivered");
+    });
+});
