930651a2c085cb59757308335611d085a5d6f20a
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-22 12:04:40 +0530

    Learned basics of async , leaned what is the need of promises callback hell etc

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//async js 
+//async js
+function stepOne() {
+  return new Promise((resolve, reject) => {
+    console.log("Step 1");
+    resolve();
+  });
+}
+function stepTwo() {
+  return new Promise((resolve, reject) => {
+    console.log("Step 2");
+    resolve();
+  });
+}
+function stepThree() {
+  return new Promise((resolve, reject) => {
+    console.log("Step 3");
+    resolve();
+  });
+}
+stepOne()
+  .then(stepTwo)
+  .then(stepThree)
+  .then(function () {
+    console.log("All Steps Done");
+  });
