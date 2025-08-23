022c8727c5315f41701096fc6b2d6d31cdeb298a
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-23 09:54:49 +0530

    From Today done with the js lectures and also understandings of js now what i will do is from today will sure to practice js from youtube by building projects to get more understandings of js then will go for the Reactjs

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ btn.addEventListener("click", function () {
   fetch("https://api.github.com/users/zuhaib-dev")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.id);
+      console.log(data.avatar_url);
     });
 });
