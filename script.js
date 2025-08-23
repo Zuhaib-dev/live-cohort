89c714c8a37460e44083031e100281d57cb570d3
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-23 08:26:45 +0530

    Learned Fetch("")

--- script.js
+++ script.js
@@ -1,49 +1,8 @@
-function getUserInfo() {
-  return new Promise((resolve, reject) => {
-    resolve({ id: 1, name: "Zuhaib" });
-  });
-}
-function getUserPic(id) {
-  return new Promise((resolve, reject) => {
-    resolve(["pic 1", "pic 2"]);
-  });
-}
-function getUserCmts(id) {
-  return new Promise((resolve, reject) => {
-    resolve(["Awesome Post", "Great Content"]);
-  });
-}
-function getUserFlowers(id) {
-  return new Promise((resolve, reject) => {
-    resolve(["10K Followers on instagram"]);
-  });
-}
-function getUserFlowing(id) {
-  return new Promise((resolve, reject) => {
-    resolve(["250 Flowing on Instagram"]);
-  });
-}
-getUserInfo()
-  .then(function (data) {
-    console.log(data);
-    return getUserPic(data.id);
-  })
-  .then(function (data) {
-    console.log(data);
-    return getUserCmts(data.id);
-  })
-  .then(function (data) {
-    console.log(data);
-    return getUserFlowers(data.id);
-  })
-  .then(function (data) {
-    console.log(data);
-    return getUserFlowing(data.id);
-  })
-  .then(function (data) {
-    console.log(data);
-  })
-  .finally(function(){
-    console.log("ALL data Fetched");
-    
-  })
+let btn = document.querySelector("#btn3");
+btn.addEventListener("click", function () {
+  fetch("https://api.github.com/users/zuhaib-dev")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.id);
+    });
+});
